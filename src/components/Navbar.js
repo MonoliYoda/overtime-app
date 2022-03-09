efeed6f265f2dd32381677023cd2665149ec5981
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-03-09 21:39:37 +0100

    Created bottom navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import React, { useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export default function Navbar() {
+  const [value, setValue] = useState();
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="History"
+          value="history"
+          icon={<ViewListIcon />}
+        />
+        <BottomNavigationAction
+          label="Stats"
+          value="statstics"
+          icon={<EqualizerIcon />}
+        />
+        <BottomNavigationAction
+          label="Settings"
+          value="settings"
+          icon={<SettingsIcon />}
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+}
