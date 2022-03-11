ade2c64cb267e86bac001abab29f5d7b615c22f7
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-03-11 13:32:28 +0100

    Dashboard rough layout done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,19 @@
 import { MoreHoriz } from "@mui/icons-material";
 import {
-  AppBar,
+  Button,
   Card,
   CardHeader,
+  CircularProgress,
+  Divider,
   Grid,
   IconButton,
   List,
   ListItem,
   ListItemText,
+  Stack,
   Typography,
 } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import withContext from "../withContext";
@@ -80,9 +84,12 @@ function Dashboard(props) {
           <Card variant="outlined">
             <CardHeader
               title={
-                <Typography variant="h4">
-                  {activeJob && activeJob.name}
-                </Typography>
+                <Stack>
+                  <Typography variant="h4">
+                    {activeJob && activeJob.name}
+                  </Typography>
+                  <Typography>12/03/2022</Typography>
+                </Stack>
               }
               action={
                 <IconButton>
@@ -90,7 +97,26 @@ function Dashboard(props) {
                 </IconButton>
               }
             ></CardHeader>
+            <Grid container alignItems="center">
+              {/* divider={<Divider orientation="vertical" flexItem />} */}
+              <Grid item xs={8}>
+                <List>
+                  <ListItem>ProjectName</ListItem>
+                  <ListItem>Start Time</ListItem>
+                  <ListItem>Start Time</ListItem>
+                  <ListItem>Overtime</ListItem>
+                </List>
+              </Grid>
+              <Grid item xs={4}>
+                <CircularProgress variant="determinate" value={75} />
+              </Grid>
+            </Grid>
           </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="center">
+            <Button variant="contained">STOP</Button>
+          </Box>
         </Grid>
         <Grid item xs={12}>
           <Card>
