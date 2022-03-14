80fe05c8b3badb965e6e9a1226a3cdedc2a00e68
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-03-14 19:44:23 +0100

    Changed hstory display to accordion

--- src/components/History.js
+++ src/components/History.js
@@ -1,32 +1,43 @@
-import { Box } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Card,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import withContext from "../withContext";
 import { minutesToTimeString } from "../util/utils";
+import { ExpandMore } from "@mui/icons-material";
 
 function History(props) {
   const fb = { ...props.value };
   const navigate = useNavigate();
-  const [localUserJobs, setLocalUserJobs] = useState([]);
-  const [processedRows, setProcessedRows] = useState([]);
+  const [expanded, setExpanded] = useState(false);
 
-  const cols = [
-    {
-      field: "startTime",
-      headerName: "Data",
-      valueFormatter: (params) => {
-        return params.value.toLocaleDateString();
-      },
-    },
-    {
-      field: "overtimeMinutes",
-      headerName: "Nadgodziny",
-      valueFormatter: (params) => {
-        return minutesToTimeString(params.value);
-      },
-    },
-  ];
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  function getFormattedOvertime(job) {
+    const endTime = job.endTime || new Date();
+    let runtimeMinutes = Math.floor((endTime - job.startTime) / 1000 / 60);
+    let ovtMinutes = runtimeMinutes - job.ovtScheme.stdHours * 60;
+    if (ovtMinutes < 0) return "00:00";
+    return minutesToTimeString(ovtMinutes);
+  }
+
+  function getFormattedWorktime(job) {
+    const endTime = job.endTime || new Date();
+    let runtimeMinutes = Math.floor((endTime - job.startTime) / 1000 / 60);
+    return minutesToTimeString(runtimeMinutes);
+  }
 
   useEffect(() => {
     if (!fb.currentUser) {
@@ -36,50 +47,67 @@ function History(props) {
     fb.fetchUserJobs();
   }, []);
 
-  useEffect(() => {
-    let isMounted = true;
-    if (isMounted) {
-      console.log("Setting localUserJobs to:", [...fb.userJobs]);
-      setLocalUserJobs([...fb.userJobs]);
-    }
-    return () => {
-      isMounted = false;
-    };
-  }, [fb.userJobs]);
-
-  useEffect(() => {
-    let isMounted = true;
-    //console.log("first");
-    let rows = [...localUserJobs];
-    rows.map((row) => {
-      if (!row.endTime) {
-        row.endTime = new Date();
-      }
-      row.runtimeMinutes = (
-        Math.floor((row.endTime - row.startTime) / 1000) / 60
-      ).toFixed(4);
-      let overtime = row.runtimeMinutes - row.ovtScheme.stdHours * 60;
-      if (overtime > 0) {
-        row.overtimeMinutes = overtime;
-      } else {
-        row.overtimeMinutes = 0;
-      }
-      return row;
-    });
-    if (isMounted) {
-      console.log("Setting processedRows to:", rows);
-      setProcessedRows(rows);
-    }
-    rows = [];
-    return () => {
-      isMounted = false;
-    };
-  }, [localUserJobs]);
-
   return (
-    <Box sx={{ height: "calc(100vh - 150px)" }}>
-      <DataGrid rows={processedRows} columns={cols}></DataGrid>
-    </Box>
+    <Card>
+      {fb.userJobs.map((job) => {
+        return (
+          <Accordion
+            key={job.id}
+            expanded={expanded === job.id}
+            onChange={handleChange(job.id)}
+          >
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                {job.name}
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                {job.startTime.toLocaleDateString()}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                divider={<Divider orientation="vertical" flexItem />}
+              >
+                <Box>
+                  <Typography>Start</Typography>
+                  <Typography>
+                    {job.startTime.toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography>
+                    {`Czas pracy: ${getFormattedWorktime(job)}`}
+                  </Typography>
+                  <Typography>
+                    {`Nadgodziny: ${getFormattedOvertime(job)}`}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography>Koniec</Typography>
+                  <Typography>
+                    {job.endTime
+                      ? job.endTime.toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })
+                      : "--:--"}
+                  </Typography>
+                </Box>
+              </Stack>
+            </AccordionDetails>
+            <AccordionActions>
+              <Button>Edytuj</Button>
+            </AccordionActions>
+          </Accordion>
+        );
+      })}
+    </Card>
   );
 }
 
